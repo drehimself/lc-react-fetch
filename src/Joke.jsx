3a1ba4818d4bbf855f3146abe3ff41330c875125
Andre Madarang <andre_madarang@hotmail.com>
2021-07-06 18:22:09 -0400

    Episode 14 - Fetching Data with custom hook

--- src/Joke.jsx
+++ src/Joke.jsx
@@ -1,28 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import useFetch from './useFetch';
 
 export default function Reddit() {
-  const [joke, setJoke] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [errorMessage, setErrorMessage] = useState(null);
-
-  useEffect(() => {
-    fetch('https://official-joke-api.appspot.com/jokes/random')
-      .then(response => response.json())
-      .then(result => {
-        setIsLoading(false);
-        setJoke(result.setup + ' ' + result.punchline);
-      })
-      .catch(error => {
-        setIsLoading(false);
-        setErrorMessage('There was an error');
-      });
-  }, []);
+  const {
+    data: joke,
+    isLoading,
+    errorMessage,
+  } = useFetch('https://official-joke-api.appspot.com/jokes/random');
 
   return (
     <div>
       <h2>Joke API</h2>
       {isLoading && <div>Loading...</div>}
-      {joke && <div>{joke}</div>}
+      {joke && <div>{joke.setup + ' ' + joke.punchline}</div>}
       {errorMessage && <div>{errorMessage}</div>}
     </div>
   );
